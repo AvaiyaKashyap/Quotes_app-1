250666c4caef928d83399bf7603fe4a2c73ede60
khushi bhadani <khushibhadani22410@gmail.com>
2023-05-02 18:41:38 +0530

    quotes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:quotes_app/view/screen/SplashPage.dart';
 import 'package:quotes_app/view/screen/quotesCategory.dart';
 import 'package:quotes_app/view/screen/quotesHomePage.dart';
 import 'package:quotes_app/view/screen/welcome.dart';
 
 void main() {
+  debugRepaintRainbowEnabled:
+  true;
   runApp(const MyApp());
 }
 
--- lib/view/screen/quotesHomePage.dart
+++ lib/view/screen/quotesHomePage.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../controller/helper/QuotesGlobal.dart';
 import '../../controller/helper/quotesApiHelper.dart';
 import '../../modal/quotesModal.dart';
@@ -12,11 +13,6 @@ class QuotesHomePage extends StatefulWidget {
 }
 
 class _QuotesHomePageState extends State<QuotesHomePage> {
-  // Random random = Random();
-  // int i = 0;
-  // changeIndex() {
-  //   setState(() => i = random.nextInt(3));
-  // }
   List<Color> bgColors = [
     Colors.white,
     Colors.red.shade200,
@@ -39,171 +35,216 @@ class _QuotesHomePageState extends State<QuotesHomePage> {
   ];
 
   bool isFav = false;
+  static GlobalKey previewContainer = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     String data = ModalRoute.of(context)!.settings.arguments as String;
     Random r1 = Random();
     int n1 = r1.nextInt(18);
     int n2 = r1.nextInt(18);
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: Text(data),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12),
-        child: FutureBuilder(
-            future: QuotesApiHelper.quotesApiHelper.fetchData(),
-            builder: (context, snp) {
-              if (snp.hasError) {
-                return Center(
-                  child: Text("ERROR:- ${snp.error}"),
-                );
-              } else if (snp.hasData) {
-                // print('${snp.data}');
-                List<Quotes>? data = snp.data;
-                return (data != null)
-                    ? ListView.builder(
-                        itemCount: data.length,
-                        itemBuilder: (context, i) {
-                          return Column(
-                            children: [
-                              Container(
-                                height: 450,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                      image:
-                                          AssetImage("${Global.bGImage[n2]}"),
-                                      fit: BoxFit.cover),
-                                  // border: Border.all(
-                                  //   width: 3,
-                                  //   // color: Colors.black,
-                                  // ),
-                                  // color: Colors.black,
-                                  borderRadius: BorderRadius.vertical(
-                                      top: Radius.circular(20)),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(16),
-                                  child: Center(
-                                    child: Text(
-                                      data[i].quote,
-                                      style: TextStyle(
-                                          color: bgColors[n1],
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 20),
+    return RepaintBoundary(
+      key: previewContainer,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text(data),
+          backgroundColor: Colors.grey.shade100,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: FutureBuilder(
+              future: QuotesApiHelper.quotesApiHelper.fetchData(),
+              builder: (context, snp) {
+                if (snp.hasError) {
+                  return Center(
+                    child: Text("ERROR:- ${snp.error}"),
+                  );
+                } else if (snp.hasData) {
+                  // print('${snp.data}');
+                  List<Quotes>? data = snp.data;
+                  return (data != null)
+                      ? ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, i) {
+                            return Column(
+                              children: [
+                                Container(
+                                  height: 450,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image:
+                                            AssetImage("${Global.bGImage[n2]}"),
+                                        fit: BoxFit.cover),
+                                    borderRadius: BorderRadius.circular((20)),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Center(
+                                      child: Text(
+                                        data[i].quote,
+                                        style: TextStyle(
+                                            color: bgColors[n1],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                height: 60,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    width: 3,
-                                    color: Colors.white,
-                                  ),
-                                  color: Colors.white,
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      color: Colors.black, blurRadius: 10,
-                                      // blurRadius: 10,
-                                    )
-                                  ],
-                                  borderRadius: const BorderRadius.vertical(
-                                      bottom: Radius.circular(20)),
+                                const SizedBox(
+                                  height: 130,
                                 ),
-                                child: Row(
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
                                   children: [
-                                    OutlinedButton.icon(
-                                      onPressed: () {},
-                                      label: const Text(
-                                        "Share",
-                                        style: TextStyle(color: Colors.grey),
-                                      ),
-                                      icon: const Icon(
-                                        Icons.share_outlined,
-                                        color: Colors.black,
-                                        size: 20,
-                                      ),
-                                    ),
-                                    OutlinedButton.icon(
-                                      onPressed: () {
-                                        setState(() {});
+                                    GestureDetector(
+                                      onTap: () {
+                                        Share.share(data[i].quote);
                                       },
-                                      label: const Text(
-                                        "Color",
-                                        style: TextStyle(color: Colors.grey),
-                                      ),
-                                      icon: const Icon(
-                                        Icons.color_lens_outlined,
-                                        color: Colors.black,
-                                        size: 20,
-                                      ),
-                                    ),
-                                    OutlinedButton.icon(
-                                        onPressed: () {},
-                                        label: const Text(
-                                          "Like",
-                                          style: TextStyle(color: Colors.grey),
+                                      child: Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            boxShadow: const [
+                                              BoxShadow(
+                                                  // color: Colors.black, blurRadius: 5,
+                                                  // blurRadius: 10,
+                                                  )
+                                            ],
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Icon(
+                                              Icons.share_outlined,
+                                              color: Colors.red.shade200,
+                                              size: 20,
+                                            ),
+                                            const Text(
+                                              "Share",
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            )
+                                          ],
                                         ),
-                                        icon: (isFav == false)
-                                            ? const Icon(
-                                                Icons.favorite_border,
-                                                color: Colors.black,
-                                                size: 20,
-                                              )
-                                            : Icon(
-                                                Icons.favorite,
-                                                color: Colors.pink.shade900,
-                                              )),
-                                    OutlinedButton.icon(
-                                      onPressed: () {
+                                      ),
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
                                         setState(() {});
                                       },
-                                      label: const Text(
-                                        "BG",
-                                        style: TextStyle(color: Colors.grey),
+                                      child: Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            boxShadow: const [
+                                              BoxShadow(
+                                                  // color: Colors.black, blurRadius: 3,
+                                                  // blurRadius: 10,
+                                                  )
+                                            ],
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Icon(
+                                              Icons.color_lens_outlined,
+                                              color: Colors.red.shade200,
+                                              size: 20,
+                                            ),
+                                            const Text(
+                                              "Color",
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                      icon: const Icon(
-                                        Icons.image_outlined,
-                                        color: Colors.black,
-                                        size: 20,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {});
+                                      },
+                                      child: Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            boxShadow: const [
+                                              BoxShadow(
+                                                  // color: Colors.black, blurRadius: 5,
+                                                  // blurRadius: 10,
+                                                  )
+                                            ],
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Icon(
+                                              Icons.image_outlined,
+                                              color: Colors.red.shade200,
+                                              size: 20,
+                                            ),
+                                            const Text(
+                                              "BG",
+                                              style:
+                                                  TextStyle(color: Colors.grey),
+                                            )
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ],
                                 ),
-                              ),
-                              Container(
-                                height: 50,
-                                width: 50,
-                                decoration: BoxDecoration(
-                                    color: Colors.pink,
-                                    border: Border.all(
-                                        width: 2, color: Colors.black)),
-                              ),
-                              Text("author: ${data[i].author}"),
-                              Text("Category: ${data[i].category}"),
-                            ],
-                          );
-                        })
-                    : const Center(
-                        child: Text(
-                          "NO DATA",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      );
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(
-                    backgroundColor: Colors.cyan,
-                    color: Colors.cyan.shade900,
-                  ),
-                );
-              }
-            }),
+                                // Container(
+                                //   height: 50,
+                                //   width: 50,
+                                //   decoration: BoxDecoration(
+                                //       color: Colors.pink,
+                                //       border: Border.all(
+                                //           width: 2, color: Colors.black)),
+                                // ),
+                                // Text("author: ${data[i].author}"),
+                                // Text("Category: ${data[i].category}"),
+                              ],
+                            );
+                          })
+                      : const Center(
+                          child: Text(
+                            "NO DATA",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        );
+                } else {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.cyan,
+                      color: Colors.cyan.shade900,
+                    ),
+                  );
+                }
+              }),
+        ),
+        backgroundColor: Colors.grey.shade100,
       ),
     );
   }
